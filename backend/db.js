ad44fe96788476dbc45d7db817a42578f3fe4155
Anoop Painuly <painulyanoop5@gmail.com>
2024-06-11 07:44:27 -0400

    database part

---
+++ backend/db.js
@@ -0,0 +1,29 @@
+/*
+title {
+    title:string,
+    discription:string,
+    completed : boolene
+}
+
+
+mongodb+srv://itachime99:<password>@todo-db.dh8zewv.mongodb.net/?retryWrites=true&w=majority&appName=todo-db
+*/
+
+const mongoose = require("mongoose");
+const { boolean } = require("zod");
+
+mongoose.connect(
+  "mongodb+srv://itachime99:dclvtNdfRfiJchp7@todo-db.dh8zewv.mongodb.net/?"
+);
+
+const todoSchema = mongoose.Schema({
+  title: String,
+  discription: String,
+  Completed: Boolean,
+});
+
+const toto = mongoose.model("todos", todoSchema);
+
+module.exports = {
+  todo,
+};
